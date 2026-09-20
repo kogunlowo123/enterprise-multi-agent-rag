701fbb425a18873780a6167e79f8a5136345e1d6
Kehinde Ogunlowo <kogunlowo@gmail.com>
2026-09-20 10:57:55 -0500

    Accept API keys as Settings constructor arguments

--- src/emrag/config.py
+++ src/emrag/config.py
@@ -26,6 +26,7 @@ class Settings(BaseSettings):
         env_file=".env",
         env_file_encoding="utf-8",
         extra="ignore",
+        populate_by_name=True,
     )
 
     # Provider selection
